cce5c31b10c75e99fef26c9e5afe7744fc767c33
Santiago Rivadeneira <santiriva1994@gmail.com>
2019-11-10 17:24:25 -0600

    Se lee el archivo csv unicamente en carpeta de proyecto y se cargan datos unicamente en un vector auxiliar de manera lineal

--- src/edd/pkg201313722_examenfinal/ArbolLinealizado.java
+++ src/edd/pkg201313722_examenfinal/ArbolLinealizado.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  *
@@ -12,28 +13,44 @@ import java.io.IOException;
 public class ArbolLinealizado {
 
     NodoArbol[] arbolLN;
+    NodoArbol[] datosCSV;
+    int cont = 0;
 
     public ArbolLinealizado() {
         this.arbolLN = new NodoArbol[100];
-        for (int i = 0; i < 10; i++) {
-            arbolLN[i] = new NodoArbol(0, " ");
+        for (int i = 0; i < arbolLN.length; i++) {
+            arbolLN[i] = new NodoArbol(-1, " ");
         }
-        leerCsv();
+        this.datosCSV = new NodoArbol[100];
+        for (int i = 0; i < datosCSV.length; i++) {
+            datosCSV[i] = new NodoArbol(-1, " ");
+        }
+        menu();
+        //insertar("2313", "safasd");
     }
 
-    public void leerCsv() {
-        String csvFile = "alumnos.csv";
+    public void leerCsv(String nombreAr) {
+        String csvFile = nombreAr.toString();
         BufferedReader br = null;
         String line;
+        int cont = 0;
         String separador = ",";
         try {
             br = new BufferedReader(new FileReader(csvFile));
             while ((line = br.readLine()) != null) {
                 String[] datos = line.split(separador);
-                if (datos[0]=="carne"|| datos[1]=="nombre") {
-                } else {
-                    System.out.println(datos[0] +"\t"+ datos[1]);
+                //datos[0] = carne ; datos[1] = nombre
+                if (!datos[0].equals("carne")) {
+                    datosCSV[cont].carnet = Integer.parseInt(datos[0]);
                 }
+                if (!datos[1].equals("nombre")) {
+                    datosCSV[cont].nombre = datos[1];
+                }
+                if (!datos[0].equals("carne") && !datos[1].equals("nombre")) {
+                    cont++;
+                }
+                //System.out.println(datos[0] +"\t"+ datos[1]);
+
             }
         } catch (FileNotFoundException e) {
             System.out.println("Error al leer archivo: " + e);
@@ -48,5 +65,74 @@ public class ArbolLinealizado {
                 }
             }
         }
+    }
+
+    public void insertar(int cnt, String nom) {
+
+        if (this.arbolLN[0].carnet == -1) {
+            this.arbolLN[0].carnet = cnt;
+            this.arbolLN[0].nombre = nom;
+        } else {
+            insertarAVL(cnt, nom);
+        }
+    }
+
+    public void insertarAVL(int cnt, String nom) {
+
+    }
+
+    public int calcularIZQ(int ind) {
+        int calc;
+        calc = (2 * ind) + 1;
+        return calc;
+    }
+
+    public int calcularDER(int ind) {
+        int calc;
+        calc = (2 * ind) + 2;
+        return calc;
+    }
+
+    public int calcularPadre(int ind) {
+        int calc;
+        calc = ((ind - 1) / 2);
+        return calc;
+    }
+
+    public void imprimir() {
+        for (int i = 0; i < datosCSV.length; i++) {
+            System.out.println("indice: " + i + "\t" + datosCSV[i].carnet + "\t" + datosCSV[i].nombre);
+        }
+    }
+
+    public void menu() {
+        int op;
+        String nomArc;
+        Scanner entrada = new Scanner(System.in);
+        do {
+            System.out.println("--------- No Me Quiero Ir Sr.Stark ---------");
+            System.out.println("1.- Carga Archivos");
+            System.out.println("2.- Generar Reportes");
+            System.out.println("3.- Salir");
+            op = entrada.nextInt();
+            switch (op) {
+                case 1:
+                    menuLeerArchivo();
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+            }
+        } while (true);
+
+    }
+
+    public void menuLeerArchivo() {
+        String nomArc;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del archivo: ");
+        nomArc = entrada.nextLine();
+        System.out.println("Cargado a vector Exitosamente");
+        leerCsv(nomArc);
     }
 }
--- src/edd/pkg201313722_examenfinal/NodoArbol.java
+++ src/edd/pkg201313722_examenfinal/NodoArbol.java
@@ -6,12 +6,18 @@ package edd.pkg201313722_examenfinal;
 public class NodoArbol {
     int carnet;
     String nombre;
+    boolean esPadre;
+    int fe;
+    int altura;
 
     public NodoArbol(int carnet, String nombre) {
         this.carnet = carnet;
         this.nombre = nombre;
+        this.esPadre = false;
+        this.fe = 0;
+        this.altura = 1;
     }
-
+    
     public int getCarnet() {
         return carnet;
     }
